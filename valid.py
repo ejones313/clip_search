f77003236730ab797fbc1ec4a10be58198438052
Milind <mjagota@stanford.edu>
2018-03-15 22:24:14 -0700

    Fixed cosine validate
    fixed cosine validate

--- valid.py
+++ valid.py
@@ -44,7 +44,7 @@ def validate_L2(word_model, vid_model, things, indices, cuda = False):
     return float(avg_prctile_pos), float(avg_dist_diff)
 
 
-def validate_cosine(word_model, vid_model, things, indices, top_perc = 20, cuda = False):
+def validate_cosine(word_model, vid_model, things, indices, cuda = False):
     vid_model.eval()
     word_model.eval()
 
@@ -58,15 +58,15 @@ def validate_cosine(word_model, vid_model, things, indices, top_perc = 20, cuda 
     word_output, word_lengths = nn.utils.rnn.pad_packed_sequence(word_output)
     vid_output, vid_lengths = nn.utils.rnn.pad_packed_sequence(vid_output)
 
-    word_order = np.zeros(word_indices.shape)
-    vid_order = np.zeros(vid_indices.shape)
+    # Unscramble output
+    word_order = torch.zeros(word_indices.shape).long()
+    vid_order = torch.zeros(vid_indices.shape).long()
     for i in range(word_indices.size()[0]):
         word_order[word_indices[i]] = i
         vid_order[vid_indices[i]] = i
-    word_output = word_output[np.array(word_lengths)-1, np.arange(word_output.shape[1]), :]
-    vid_output = vid_output[np.array(vid_lengths)-1, np.arange(vid_output.shape[1]), :]
-    word_unscrambled = word_output[word_order,:].data.numpy()
-    vid_unscrambled = vid_output[vid_order,:].data.numpy()
+
+    word_unscrambled = utils.unscramble(word_output, word_lengths, word_order, word_indices.size()[0], cuda)
+    vid_unscrambled = utils.unscramble(vid_output, vid_lengths, vid_order, word_indices.size()[0], cuda)
 
     word_norm_scale = np.linalg.inv(np.diag(np.linalg.norm(word_unscrambled, axis=1)))
     vid_norm_scale = np.linalg.inv(np.diag(np.linalg.norm(vid_unscrambled, axis=1)))
